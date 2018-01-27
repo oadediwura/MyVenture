016671bb4ae3c21f384c035d4256206df90fc19e
zudevon <devonrasch@live.com>
2018-01-27 12:37:32 -0600

    the start "myVenture"

---
+++ microBlog.py
@@ -0,0 +1,10 @@
+from app import app
+app.config.from_object('config')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+"""""SCRIPT = /Users/a/myProgs/microBlog/microBlog.py"""
+""" ~/anaconda/bin/python """
